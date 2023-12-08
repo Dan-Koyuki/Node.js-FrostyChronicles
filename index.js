382bbb3c51be99f8bedd786797dc9b193f59ac0f
Dan-Koyuki <dankoyuki398@gmail.com>
2023-12-08 15:39:51 +0700

    idk

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.options('*', cors(corsOptions));
+
 // auth route
 app.use("/api/register", register);
 app.use("/api/login", login);
