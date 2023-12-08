b02e6be9a5c47460d9f48d94bf61a3786c4acbf8
Dan-Koyuki <dankoyuki398@gmail.com>
2023-12-08 13:33:16 +0700

    cors fix

--- utils/corsMiddleware.js
+++ utils/corsMiddleware.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://frontend-frosty.vercel.app',
+  origin: 'https://frontend-frosty.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
