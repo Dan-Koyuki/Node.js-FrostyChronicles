2dceca27310eb43d19195b661ff6905eb4c247f3
Dan-Koyuki <dankoyuki398@gmail.com>
2023-12-08 13:57:18 +0700

    fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const pokemons = require('./models/Pokemon');
 
 const app = express();
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://frontend-frosty.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
