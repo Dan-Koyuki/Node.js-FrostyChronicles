7cf4385ffb25b9140d8ee081523613261cf4c1a4
Dan-Koyuki <dankoyuki398@gmail.com>
2023-12-09 06:59:36 +0700

    pokemon add to team fix

--- routes/addMember.js
+++ routes/addMember.js
@@ -8,24 +8,6 @@ router.post('/', async (req, res) => {
   const schema = joi.object({
     teamID: joi.string().required(),
     pokemonName: joi.string().required(),
-    ability: joi.string(),
-    item: joi.string(),
-    ivHP: joi.number().min(0).max(31),
-    ivATK: joi.number().min(0).max(31),
-    ivDEF: joi.number().min(0).max(31),
-    ivSPA: joi.number().min(0).max(31),
-    ivSPD: joi.number().min(0).max(31),
-    ivSPE: joi.number().min(0).max(31),
-    evHP: joi.number().min(0).max(252),
-    evATK: joi.number().min(0).max(252),
-    evDEF: joi.number().min(0).max(252),
-    evSPA: joi.number().min(0).max(252),
-    evSPD: joi.number().min(0).max(252),
-    evSPE: joi.number().min(0).max(252),
-    moves1: joi.string(),
-    moves2: joi.string(),
-    moves3: joi.string(),
-    moves4: joi.string()
   });
 
   const { error } = schema.validate(req.body);
@@ -37,24 +19,6 @@ router.post('/', async (req, res) => {
   member = new Member({
     teamID: req.body.teamID,
     pokemonName: req.body.pokemonName,
-    ability: req.body.ability,
-    item: req.body.item,
-    ivHP: req.body.ivHP,
-    ivATK: req.body.ivATK,
-    ivDEF: req.body.ivDEF,
-    ivSPA: req.body.ivSPA,
-    ivSPD: req.body.ivSPD,
-    ivSPE: req.body.ivSPE,
-    evHP: req.body.evHP,
-    evATK: req.body.evATK,
-    evDEF: req.body.evDEF,
-    evSPA: req.body.evSPA,
-    evSPD: req.body.evSPD,
-    evSPE: req.body.evSPE,
-    moves1: req.body.moves1,
-    moves2: req.body.moves2,
-    moves3: req.body.moves3,
-    moves4: req.body.moves4
   });
 
   member = await member.save();
