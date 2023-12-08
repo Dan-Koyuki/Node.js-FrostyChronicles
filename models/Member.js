6c9e938a6c803f2f24a5f091b060286499574126
Dan-Koyuki <dankoyuki398@gmail.com>
2023-12-09 06:43:57 +0700

    pokemon add to team fix

--- models/Member.js
+++ models/Member.js
@@ -3,102 +3,89 @@ const mongoose = require("mongoose");
 const memberSchema = new mongoose.Schema({
   teamID: {
     type: String,
-    required: true
+    required: true,
   },
   pokemonName: {
     type: String,
-    required: true
+    required: true,
   },
   ability: {
     type: String,
-    required: true
   },
   item: {
-    type: String
+    type: String,
   },
   ivHP: {
     type: Number,
-    required: true,
     min: 0,
-    max: 31
+    max: 31,
   },
   ivATK: {
     type: Number,
-    required: true,
     min: 0,
-    max: 31
+    max: 31,
   },
   ivDEF: {
     type: Number,
-    required: true,
     min: 0,
-    max: 31
+    max: 31,
   },
   ivSPA: {
     type: Number,
-    required: true,
     min: 0,
-    max: 31
+    max: 31,
   },
   ivSPD: {
     type: Number,
-    required: true,
     min: 0,
-    max: 31
+    max: 31,
   },
   ivSPE: {
     type: Number,
-    required: true,
     min: 0,
-    max: 31
+    max: 31,
   },
   evHP: {
     type: Number,
-    required: true,
     min: 0,
-    max: 252
+    max: 252,
   },
   evATK: {
     type: Number,
-    required: true,
     min: 0,
-    max: 252
+    max: 252,
   },
   evDEF: {
     type: Number,
-    required: true,
     min: 0,
-    max: 252
+    max: 252,
   },
   evSPA: {
     type: Number,
-    required: true,
     min: 0,
-    max: 252
+    max: 252,
   },
   evSPD: {
     type: Number,
-    required: true,
     min: 0,
-    max: 252
+    max: 252,
   },
   evSPE: {
     type: Number,
-    required: true,
     min: 0,
-    max: 252
+    max: 252,
   },
   moves1: {
-    type: String
+    type: String,
   },
   moves2: {
-    type: String
+    type: String,
   },
   moves3: {
-    type: String
+    type: String,
   },
   moves4: {
-    type: String
+    type: String,
   },
 });
 
